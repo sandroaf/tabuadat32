ed113ef7b9f8dd9d735d950e3fbe6fccf742a35a
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2026-09-09 21:30:44 -0300

    Exemplo Tabuada Simples

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabuada Simples 0-10</title>
+</head>
+<body>
+    <h1>Tabadua de 0-10</h1>
+    <form action="#" method="GET">
+         <label for="fnum">Tabuade de: </label>
+         <input type="number" id="fnum" name="fnum">
+         <br>
+         <button type="submit">Tabuada</button>
+    </form>
+    <br>
+    <?php 
+        if (isset($_GET["fnum"])) {
+            $num = $_GET["fnum"];
+            echo "<table>";
+            echo "<tr><th colspan='3'>Tabuada de $num</th></tr>";
+            for($i = 0;$i <= 10;$i++) {
+                echo "<tr><td>$i X $num</td><td> = </td><td>".$i * $num."</td></tr>";
+            }
+            echo "</table>";
+        }   
+    ?>
+
+</body>
+</html>
